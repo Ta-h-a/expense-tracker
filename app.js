d1327ce823d77cba797980c9b7ee1295323434c8
Ta-h-a <tahasindoli@gmail.com>
2023-07-09 00:01:43 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express  = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 const { log } = require("console");
+const ejs = require("ejs")
 
 const app = express()
 
+app.set('view engine','ejs')
+app.set("views",__dirname+"/views")
 
 app.use(express.static("public"));
-
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.set('view engine','ejs')
 
 // Database
 
