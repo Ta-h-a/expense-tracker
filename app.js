149ad5c280c0eafb1a8563ad64bfe94366e24306
Ta-h-a <tahasindoli@gmail.com>
2023-07-11 20:35:39 +0530

    Fixed font size bug in budgets page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express()
 app.set('view engine','ejs')
 app.set("views",__dirname+"/views")
 
-app.use(express.static("public")); 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
